3eda97161910e6d019fdb6f2ef383dca84a00865
Rafael Caricio <rafael.jacinto@gmail.com>
2012-08-22 02:06:14 -0300

    Simple project as example.

---
+++ app/app.js
@@ -0,0 +1,14 @@
+Ext.application({
+    name: 'DerpCam',
+    views: ['Main'],
+
+    launch: function() {
+        console.log('mainLaunch');
+        Ext.create('DerpCam.view.Image', {
+            title: 'DerpCam Manual',
+            description: 'Foto para tirar onda com Fabio!',
+            src: 'http://thumbor.caricio.com/unsafe/filters:watermark(http://thumbor.caricio.com/unsafe/100x0/i0.kym-cdn.com/entries/icons/original/000/002/252/me-gusta.jpg,-100,150,0):watermark(http://thumbor.caricio.com/unsafe/100x0/factspy.net/wp-content/uploads/2012/02/sweet-jesus-have-mercy.jpg,-1,90,0)/fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-snc6/225176_508794262468673_1868400681_n.jpg',
+            fullscreen: true
+        });
+    }
+});
